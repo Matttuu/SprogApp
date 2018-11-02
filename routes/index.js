be64c4d502109b1a98ebab15277a4e3d4bd55173
Meblom <xuniqh@gmail.com>
2018-11-02 13:43:31 +0100

    Jeg comitter for Thomas' skyld, der er dog fejl i koden, men der er 100p fokus på at få udrettet dem :-)

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
+  res.render('index',  { 
     title: 'Express',
+    layout: 'other',
     dagens_citat: "Skyd ikke bjørnen før noget er solgt."
    });
 });
