8c56f30714712232341452e38244c656731003e2
Michael Trans <mich282q@edu.easj.dk>
2018-11-06 12:46:28 +0100

    lavet en tom chat side.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var profileRouter = require('./routes/profile');
 var ordbogRouter = require('./routes/ordbog');
 var loginRouter = require ('./routes/login');
 var signupRouter = require ('./routes/signup');
-
+var chatRouter = require ('./routes/chat');
 
 var app = express();
 
@@ -60,6 +60,7 @@ app.use('/profile', profileRouter);
 app.use('/ordbog', ordbogRouter);
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
