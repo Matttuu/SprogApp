aad056427ca659a7959fa743822fbd87c8452cd4
dani832m <31951102+dani832m@users.noreply.github.com>
2018-11-07 13:30:25 +0100

    Support-side fixet + små ændringer

--- routes/support.js
+++ routes/support.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../public/javascripts/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('support', { 
-    });
+    User.findById(req.session.userId)
+    .exec(function (error, user) {
+    res.render('support', { 
+      name: user.username
+     });
+  });
 });
 
 module.exports = router;
