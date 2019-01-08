914bfac13750b0cf50a4be600fadd25caec90221
Michael Trans <mich282q@edu.easj.dk>
2019-01-08 11:29:56 +0100

    test video

--- routes/videobog.js
+++ routes/videobog.js
@@ -214,7 +214,7 @@ router.get('/video/:filename', (req, res) => {
     }
 
     // Check if Video
-    if (file.contentType === 'video/mp3' || file.contentType === 'video/mp4' || file.contentType === 'video/mov' || file.contentType === 'video/mpeg-4' || file.contentType === 'video/x-m4a' || file.contentType === 'video/m4a' || file.contentType === 'video/amr'|| file.contentType === 'video/quicktime' || file.contentType === 'video/avi' || file.contentType === 'video/wmv' || file.contentType === 'video/RealVideo' || file.contentType === 'video/flash' || file.contentType === 'video/ogg' || file.contentType === 'video/webm') {
+    if (file.contentType === 'video/mp3' || file.contentType === 'video/mp4' || file.contentType === 'video/mov' || file.contentType === 'video/mpeg-4' || file.contentType === 'video/x-m4a' || file.contentType === 'video/m4a' || file.contentType === 'video/amr'|| file.contentType === 'video/quicktime' || file.contentType === 'video/avi' || file.contentType === 'video/wmv' || file.contentType === 'video/RealVideo' || file.contentType === 'video/flash' || file.contentType === 'video/ogg' || file.contentType === 'video/webm'|| file.contentType === 'video/H.264') {
       // Read output to browser
       const readstream = gfs.createReadStream(file.filename);
       readstream.pipe(res);
