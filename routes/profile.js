a067202c6c6221a18537eb49660e062511d44d9d
Meblom <xuniqh@gmail.com>
2019-01-09 14:13:27 +0100

    Nået længere med cookie

--- routes/profile.js
+++ routes/profile.js
@@ -9,15 +9,16 @@ app.use(cookieParser());
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  /* // Hvis der allerede er en cookie som hedder userPoints, brug værdien fra den til deadline
+   // Hvis der allerede er en cookie som hedder userPoints, brug værdien fra den til deadline
   
-  var cookie = req.cookies.name;
+  var cookie = req.cookies.userPoints;
 
-  if (cookie &&  cookie === ('userPoints')) {
+  if (cookie === ('userPoints')) {
   // få deadline fra cookien
-  var deadline = cookie.match(/(^|;)userPoints=([^;]+)/)[2];
- 
-  console.log('Cookie not create');
+  
+  console.log(req.cookies.userPoints);
+  console.log(req.cookies);
+  console.log(deadline);
 }
 
 // ellers, sæt en deadline fra nu og
@@ -30,14 +31,17 @@ else {
   var currentTime = Date.parse(new Date());
   var deadline = new Date(currentTime + timeInMinutes * 60 * 1000);
   expirationGMT = deadline.toGMTString();
+  var rescookie = expirationGMT.replace(/%/g, " ");
 
   // Gem i deadline i en cookie
-  res.cookie('userPoints', deadline, {expires: deadline, domain: 'localhost'});
+  res.cookie('userPoints', rescookie, {expires: deadline, domain: 'localhost', encode: String});
   console.log('Cookie created');
-  console.log(expirationGMT);
-  console.log(deadline);
+  console.log(rescookie)
+  console.log(req.cookies.userPoints)
+  console.log(req.cookies);
+  //
   
-} */
+} 
 
   User.findById(req.session.userId)
   .exec(function (error, user) {
@@ -51,39 +55,10 @@ else {
     sprogmakker: user.role === "Sprogmakker",
     kursist: user.role === "Kursist",
     admin: user.role === "Administrator",
-    ordJongloer: user.userRank === "Ord-Jonglør"
+    ordJongloer: user.userRank === "Ord-Jonglør",
+    cookieTest: rescookie
    });
 })
-
-app.use(function (req , res, next) {
-  var cookie = req.cookies.userPoints;
-
-  if (cookie && cookie.match('userPoints')) {
-  // få deadline fra cookien
-  var deadline = cookie.match(/(^|;)userPoints=([^;]+)/)[2];
-  res.cookie('userPoints', deadline, {expires: expirationGMT, domain: 'localhost'});
-  console.log('Cookie not create');
-}
-
-// ellers, sæt en deadline fra nu og
-// gem som cookie med givet navn
-else {
-
-  
-  // Lave en deadline i 'x' minutter fra dette tidspunkt.
-  var timeInMinutes = 1320;
-  var currentTime = Date.parse(new Date());
-  var deadline = new Date(currentTime + timeInMinutes * 60 * 1000);
-  expirationGMT = deadline.toGMTString();
-
-  // Gem i deadline i en cookie
-  res.cookie('userPoints', deadline, {expires: expirationGMT, domain: 'localhost'});
-  res.send('');
-  console.log('Cookie created');
-  
-}
-next();
-});
 });
 
 
