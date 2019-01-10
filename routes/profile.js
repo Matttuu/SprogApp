cbd433bee7f3bda0539869715939e53c21522c20
Thom9521 <Thom9521@edu.easj.dk>
2019-01-10 20:23:03 +0100

    Tilføjet kodekommentar

--- routes/profile.js
+++ routes/profile.js
@@ -6,43 +6,43 @@ const mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+
+  mongoose.connect('mongodb://admin:team12@ds125693.mlab.com:25693/cdi', { useNewUrlParser: true, }, function (err, db) {
+    if (err) { throw err; }
+    var collection = db.collection('users');
+    var d = new Date();
+    var getDate = d.getDate();
+    /*Tjekker tjekker hvornår brugeren sidst har været logget ind.
+      Hvis ikke man har været logget ind i dag så får man 10 point,
+      og værdien for sidste besøg bliver opdateret.*/
+    if (user.lastVisitDate != getDate) {
+      collection.update({ 'uniqueId': user.uniqueId },
+        { '$set': { 'lastVisitDate': getDate } });
+      collection.update({ 'uniqueId': user.uniqueId },
+        { '$inc': { 'userPoints': 10 } });
+    }
+  });
+
   User.findById(req.session.userId)
     .exec(function (error, user) {
-
-
-        res.render('profile', {
-          title: 'Dashboard',
-          user: user,
-          username: user.username,
-          role: user.role,
-          userPoints: user.userPoints,
-          userRank: user.userRank,
-          uniqueId: user.uniqueId,
-          sprogmakker: user.role === "Sprogmakker",
-          kursist: user.role === "Kursist",
-          admin: user.role === "Administrator",
-          ingenRang: user.userRank === "Ingen rang",
-          bronze: user.userRank === "Bronze",
-          sølv: user.userRank === "Sølv",
-          guld: user.userRank === "Guld",
-          platin: user.userRank === "Platin",
-          diamant: user.userRank === "Diamant",
-          ordJongloer: user.userRank === "Ord-Jonglør",
-        });
-        
-      mongoose.connect('mongodb://admin:team12@ds125693.mlab.com:25693/cdi', { useNewUrlParser: true, }, function (err, db) {
-        if (err) { throw err; }
-        var collection = db.collection('users');
-        var d = new Date();
-        var getDate = d.getDate();
-        console.log(getDate);
-        console.log(user.lastVisitDate);
-        if (user.lastVisitDate != getDate) {
-          collection.update({ 'uniqueId': user.uniqueId },
-            { '$set': { 'lastVisitDate': getDate } });
-          collection.update({ 'uniqueId': user.uniqueId },
-            { '$inc': { 'userPoints': 10 } });
-        }
+      res.render('profile', {
+        title: 'Dashboard',
+        user: user,
+        username: user.username,
+        role: user.role,
+        userPoints: user.userPoints,
+        userRank: user.userRank,
+        uniqueId: user.uniqueId,
+        sprogmakker: user.role === "Sprogmakker",
+        kursist: user.role === "Kursist",
+        admin: user.role === "Administrator",
+        ingenRang: user.userRank === "Ingen rang",
+        bronze: user.userRank === "Bronze",
+        sølv: user.userRank === "Sølv",
+        guld: user.userRank === "Guld",
+        platin: user.userRank === "Platin",
+        diamant: user.userRank === "Diamant",
+        ordJongloer: user.userRank === "Ord-Jonglør",
       });
     })
 });
