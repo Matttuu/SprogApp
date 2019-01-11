fc6f9b9771ab63dd71235de45f40523ab27eafa0
Thom9521 <Thom9521@edu.easj.dk>
2019-01-11 15:12:38 +0100

    Fjernet en masse kode, som ikke blev brugt + kodekommentarer

--- routes/billedbog.js
+++ routes/billedbog.js
@@ -18,15 +18,10 @@ const conn = mongoose.createConnection(mongoURI, { useNewUrlParser: true });
 // Init gfs
 let gfs;
 
-// Init gfs
-let gfsLyd;
-
 conn.once('open', () => {
   // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('imageuploads');
-  gfsLyd = Grid(conn.db, mongoose.mongo);
-  gfsLyd.collection('audiouploads');
 });
 
 // Create storage engine
@@ -54,27 +49,6 @@ const storage = new GridFsStorage({
   }
 });
 const upload = multer({ storage });
-
-// Create storage engine
-const storageLyd = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'audiouploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-const uploadLyd = multer({ storageLyd });
 
 // @route GET /
 // @desc Loads form
@@ -108,8 +82,8 @@ router.get('/', (req, res) => {
                   var obj5 = {};
                   obj5.users = result;
 
-                      gfs.files.find().toArray((err, files) => {
-                   // gfsLyd.files.find().toArray((err, filesLyd) => {
+                  gfs.files.find().toArray((err, files) => {
+                    // gfsLyd.files.find().toArray((err, filesLyd) => {
                     // Check if files
                     if (!files || files.length === 0) {
                       res.render('billedbog', { files: false });
@@ -118,8 +92,6 @@ router.get('/', (req, res) => {
                         user: user,
                         uniqueId: user.uniqueId,
                         files: files,
-                        //audiofiles: filesLyd,
-                        //checkAliases: req.body.audioAliases == files.filename,
                         title: 'Billedordbog',
                         sprogmakker: user.role === "Sprogmakker",
                         kursist: user.role === "Kursist",
@@ -141,10 +113,9 @@ router.get('/', (req, res) => {
                         obj4,
                         obj5
                       });
-                   }
-                 // });
+                    }
+                  });
                 });
-              });
               });
             });
           });
@@ -347,114 +318,5 @@ router.delete('/files/:id', (req, res) => {
     res.redirect('/billedbog');
   });
 });
-
-
-// LYD ****************************************************************************'
-router.post('/audiofiles/:filename', (req, res, next) => {
-
-  // Connect til database
-  mongoose.connect('mongodb://admin:team12@ds125693.mlab.com:25693/cdi',{useNewUrlParser: true,}, function(err, db){
-  if(err){throw err;}
-
-  // Oprette nyt promise som bliver kørt senere i koden.
-    function resolveDetteBagefter() {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve(res.redirect('/billedbog'));
-        }, 0001);
-      });
-    }
-    // Opretter async funktion 
-    async function asyncCall () {
-      var result = await resolveDetteBagefter();
-    // Peger på database collection 
-      var collection = db.collection('uploads.files')
-    // Bruger collection.update metoden for at opdatere / give text til specifikt billede
-      collection.update(
-      { filename: req.params.filename}, 
-      { '$set': {'audio': req.body.audio}}  
-      )
-    }
-  asyncCall();
- });
-}); 
-
-// @route POST /upload
-// @desc  Uploads file to DB
-router.post('/audioupload', uploadLyd.single('file'), (req, res) => {
-  // res.json({ file: req.file });
-  res.redirect('/billedbog');
-}); 
-
-
-// @route GET /files
-// @desc  Display all files in JSON
-router.get('/audiofiles', (req, res) => {
-  gfsLyd.files.find().toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: 'No files exist'
-      });
-    }
-
-    // Files exist
-    return res.json(files);
-  });
-});
-
-// @route GET /files/:filename
-// @desc  Display single file object
-router.get('/audiofiles/:filename', (req, res) => {
-  gfsLyd.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
-    // File exists
-    return res.json(file);
-
-  });
-});
-
-// @route GET /audio/:filename
-// @desc Display Audio
-router.get('/audio/:filename', (req, res) => {
-  gfsLyd.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
-
-    // Check if audio
-    if (file.contentType === 'audio/mp3' || file.contentType === 'audio/mp4' || file.contentType === 'audio/x-m4a' || file.contentType === 'audio/amr' || file.contentType === 'audio/wav') {
-      // Read output to browser
-      const readstream = gfsLyd.createReadStream(file.filename);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: 'Not Audio'
-      });
-    }
-  });
-});
-
-// @route DELETE /files/:id
-// @desc  Delete file
-
-router.delete('/audiofiles/:id', (req, res) => {
-  gfsLyd.remove({ _id: req.params.id, root: 'audiouploads' }, (err, gridStore) => {
-    if (err) {
-      return res.status(404).json({ err: err });
-    }
-
-    res.redirect('/billedbog');
-  });
-});
-
 
 module.exports = router;
